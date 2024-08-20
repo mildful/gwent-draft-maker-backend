612f0aa08e79a82dbecaeb05bf1bfdd820f1bdd4
Clement Flodrops <clement.flodrops@gmail.com>
2024-08-20 17:11:42 +0200

    feat: setup basic css pipeline

--- src/infrastructure/api/hypermedia/templates/Layout.tsx
+++ src/infrastructure/api/hypermedia/templates/Layout.tsx
@@ -5,7 +5,13 @@ export default function Layout({ content }: { content: JSX.Element }): JSX.Eleme
     <html>
       <head>
         <title>Gwent Draft Maker</title>
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src https://unpkg.com/htmx.org@2.0.2; child-src 'none'"></meta>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="default-src 'self'; script-src https://unpkg.com/htmx.org@2.0.2; style-src 'self' 'sha256-bsV5JivYxvGywDAZ22EZJKBFip65Ng9xoJVLbBg7bdo=', child-src 'none'"
+        ></meta>
+        <link
+          rel="stylesheet"
+          href="main.css" type="text/css"></link>
         <script src="https://unpkg.com/htmx.org@2.0.2" integrity="sha384-Y7hw+L/jvKeWIRRkqWYfPcvVxHzVzn5REgzbawhxAuQGwX1XWe70vji+VSeHOThJ" crossorigin="anonymous"></script>
       </head>
 
